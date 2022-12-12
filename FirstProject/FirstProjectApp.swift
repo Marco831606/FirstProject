97f3ad376e412e29e4873f7aeaae4c42fe931b73
marcomolisano <fair.geht.vor2013@gmail.com>
2022-12-12 11:09:11 +0100

    Initial Commit

---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by Marco Molisano on 12.12.22.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
